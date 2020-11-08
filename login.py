c845fe2b2938ba0f38dba606373ff592deed6e10
mleins60 <mleins60@gmail.com>
2020-11-08 15:44:06 +0100

    Implementation

--- login.py
+++ login.py
@@ -3,3 +3,17 @@ import twill.commands as twill
 twill.go('https://www.strato.de/apps/CustomerService#/skl')
 
 twill.showforms()
+
+#get username 
+with open("creds.txt", "r") as text_file:
+    data = text_file.readlines()
+
+print("username: ", data[0])
+print("password: ", data[1])
+
+twill.fv("LoginForm", "login", data[0])
+twill.fv("LoginForm", "login", data[1])
+
+twill.submit()
+
+print("logged in!")
